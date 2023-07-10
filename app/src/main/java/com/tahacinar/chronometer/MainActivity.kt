38efc78c64f9f7a56b81ac33fdb267deaefecd7e
Muhammed Taha Çınar <tahacinar@outlook.com.tr>
2023-07-11 00:21:51 +0300

    v.1.0.

--- app/src/main/java/com/tahacinar/chronometer/MainActivity.kt
+++ app/src/main/java/com/tahacinar/chronometer/MainActivity.kt
@@ -11,7 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
     }
 }
